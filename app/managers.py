1069e04ffc395645077927a78e4dbbe3c93e240e
Vlad Hurko <vlad22.2000@gmail.com>
2025-08-04 21:31:09 +0100

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,60 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str):
+        self.db_name = db_name
+        self.table_name = table_name
+        self._initialize_table()
+
+    def _initialize_table(self):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                CREATE TABLE IF NOT EXISTS {self.table_name} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    first_name TEXT NOT NULL,
+                    last_name TEXT NOT NULL
+                )
+            ''')
+            conn.commit()
+
+    def create(self, first_name: str, last_name: str):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                INSERT INTO {self.table_name} (first_name, last_name)
+                VALUES (?, ?)
+            ''', (first_name, last_name))
+            conn.commit()
+
+    def all(self) -> list[Actor]:
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                SELECT id, first_name, last_name FROM {self.table_name}
+            ''')
+            rows = cursor.fetchall()
+            return [
+                Actor(id=row[0], first_name=row[1], last_name=row[2])
+                for row in rows
+            ]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                UPDATE {self.table_name}
+                SET first_name = ?, last_name = ?
+                WHERE id = ?
+            ''', (new_first_name, new_last_name, pk))
+            conn.commit()
+
+    def delete(self, pk: int):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                DELETE FROM {self.table_name}
+                WHERE id = ?
+            ''', (pk,))
+            conn.commit()
